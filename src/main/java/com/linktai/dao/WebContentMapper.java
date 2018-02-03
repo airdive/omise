7478c266318eb3e2feb0d22e661ae8ced94cda8d
liuchuang <chuang.liu@bqjr.cn>
2018-02-03 15:24:20 +0800

    init...

--- src/main/java/com/linktai/dao/WebContentMapper.java
+++ src/main/java/com/linktai/dao/WebContentMapper.java
@@ -1,13 +1,14 @@
 package com.linktai.dao;
 
 import com.linktai.pojo.WebContent;
+import org.apache.ibatis.annotations.Param;
 
 public interface WebContentMapper {
     int insert(WebContent record);
 
     int insertSelective(WebContent record);
     
-    WebContent selectByName(String name);
+    WebContent selectByName(@Param("name") String name);
     
     Integer updateWebContent(WebContent webContent);
 }
